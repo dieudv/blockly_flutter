f21ac7e6ec810e3f7ceee5e49d723ab940a84d3c
Dieu Master <dieu.cit@gmail.com>
2019-10-17 17:43:56 +0700

    Clean

--- ios/Classes/BlocklyFlutterPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "BlocklyFlutterPlugin.h"
-#import <blockly_flutter/blockly_flutter-Swift.h>
-
-@implementation BlocklyFlutterPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftBlocklyFlutterPlugin registerWithRegistrar:registrar];
-}
-@end
